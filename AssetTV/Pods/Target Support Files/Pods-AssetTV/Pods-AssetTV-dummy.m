a3a7ca245cb0ceb4b4ddb974e212dc6203db3b3f
Alexandre Malkov <sm1030@gmail.com>
2016-11-30 02:49:42 +0000

    Project converted into Workspace with PODS. From now on DO NOT use project! Only use Workspace

---
+++ AssetTV/Pods/Target Support Files/Pods-AssetTV/Pods-AssetTV-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AssetTV : NSObject
+@end
+@implementation PodsDummy_Pods_AssetTV
+@end
---
+++ AssetTV/Pods/Target Support Files/Pods-AssetTVUITests/Pods-AssetTVUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AssetTVUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AssetTVUITests
+@end
